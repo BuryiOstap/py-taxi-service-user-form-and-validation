9a0a6a7453073df3715e407195bada6e99593ddc
Ostap <ostap.burij@gmail.com>
2026-01-14 23:59:58 +0200

    Solutin

--- taxi/urls.py
+++ taxi/urls.py
@@ -13,6 +13,10 @@ from .views import (
     ManufacturerCreateView,
     ManufacturerUpdateView,
     ManufacturerDeleteView,
+    DriverCreateView,
+    DriverDeleteView,
+    DriverLicenseUpdateView,
+    AddRemoveUserCarView,
 )
 
 urlpatterns = [
@@ -46,6 +50,24 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path(
+        "drivers/create/", DriverCreateView.as_view(), name="driver-create"
+    ),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
+    ),
+    path(
+        "drivers/<int:pk>/update/",
+        DriverLicenseUpdateView.as_view(),
+        name="driver-update"
+    ),
+    path(
+        "cars/<int:pk>/add-remove-user/",
+        AddRemoveUserCarView.as_view(),
+        name="add-remove-user"
+    )
 ]
 
 app_name = "taxi"
